40dbe5de72f97b3eb5892d1263d64d119f6d3221
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-21 20:18:49 -0700

    Added more pizzas to seed data to better assess the get_top_ten method

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -4,5 +4,8 @@ class Pizza < ApplicationRecord
   validates :crust, presence: true
   validates :name, presence: true, uniqueness: true
 
-
+  def self.get_top_ten(crust)
+    pizzas_of_crust_type = Pizza.where(crust: crust).order("id DESC")
+    return pizzas_of_crust_type.limit(10)
+  end
 end
