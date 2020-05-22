ed721c22cf9cfe3ff6b7ece26c56cdb61f26d901
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-21 20:44:27 -0700

    Added get_spotlight method to Pizza model and changed views/controllers to use new logic

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -3,8 +3,10 @@ class HomepagesController < ApplicationController
     if params[:user_id]
       @user = User.find_by(id: params[:user_id])
       @pizzas = Pizza.all
+      @spotlight = Pizza.get_spotlight
     else
       @pizzas = Pizza.all
+      @spotlight = Pizza.get_spotlight
     end
   end
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -5,7 +5,18 @@ class Pizza < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def self.get_top_ten(crust)
-    pizzas_of_crust_type = Pizza.where(crust: crust).order("id DESC")
-    return pizzas_of_crust_type.limit(10)
+    pizzas_of_crust_type = Pizza.where(crust: crust)
+    # if pizzas_of_crust_type.first.....has votes
+    #   return pizzas_of_crust_type.order("votes DESC").limit(10)
+    # else
+      return pizzas_of_crust_type.order("id DESC").limit(10)
+    # end
+  end
+
+  def self.get_spotlight
+    #TODO: change id DESC to votes DESC
+    top_ten_pizzas = Pizza.all.order("id DESC").limit(10)
+    #TODO: randomly select of the top ten votes?
+    return top_ten_pizzas.first
   end
 end
