25b435671189cd4d7bb852c6a352f4743ab16230
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-22 18:59:03 -0700

    Added custom login/logout routes for Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  root to: 'homepages#index'
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 
+  root to: 'homepages#index'
+  
   resources :pizzas
   resources :users do
     resources :votes, only: [:index, :create]
