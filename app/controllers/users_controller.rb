3b880c2413ecae5024ba4e53c7f22ac3e2cacdcd
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-22 20:14:26 -0700

    Altered buttons in application.html.erb nav to reflect the session status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def login
-  #   user = User.find_by(username: params[:user][:username])
-
-  #   if user.nil?
-  #     #create new user
-  #     user = User.new(username: params[:user][:username])
-  #     if !user.save
-  #       flash[:error] = "Unable to login"
-  #       redirect_to root_path
-  #       return
-  #     end
-  #     flash[:success] = "Welcome back, #{user.username}!"
-  #   else
-  #     flash[:success] = "Successfully logged in as #{user.username}."
-  #   end
-  #   session[:user_id] = user.id
-  #   redirect_to root_path
-  # end
-
   def login
     username = params[:user][:username]
     user = User.find_by(username: username)
@@ -39,8 +20,20 @@ class UsersController < ApplicationController
   end
 
   def logout
-    session[:user_id] = nil
-    flash[:success] = "You have successfully logged out."
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      unless user.nil?
+        session[:user_id] = nil
+        flash[:notice] = "Logout successful -- see you later, #{user.username}!"
+        redirect_to root_path
+        return
+      else
+        session[:user_id] = nil
+        flash[:error] = "There was an error -- Unknown User"
+      end
+    else
+      flash[:error] = "You must be logged in if you want to log out..."
+    end
     redirect_to root_path
     return
   end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe UsersController do
-  describe "session" do
+  describe "login" do
     it "can get the login form" do
       get login_path
 
@@ -19,7 +19,7 @@ describe UsersController do
 
       expect(user).wont_be_nil
       expect(session[:user_id]).must_equal user.id
-      expect(user.username).must_equal user_hash[:user][:username]
+      expect(user.username).must_equal "Test User"
     end
 
     it "can login an existing user" do
@@ -30,6 +30,17 @@ describe UsersController do
       }.wont_change "User.count"
 
       expect(session[:user_id]).must_equal user.id
+    end
+  end
+
+  describe "logout" do
+    it "can logout a logged in user" do
+      login()
+      expect(session[:user_id]).wont_be_nil
+
+      post logout_path
+
+      expect(session[:user_id]).must_be_nil
     end
   end
 end
