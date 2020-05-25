bfb14ddf725232b5eccf91cab361cef992c8ba91
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-25 16:05:31 -0700

    Added validations and strong params to Vote model

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,14 @@
 class VotesController < ApplicationController
+  def index
+  end
+
+  def create
+
+  end
+
+  private
+
+  def vote_params
+    return params.require(:vote).permit(:pizza_id, :user_id)
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :pizza
+
+  validates :pizza_id, presence: true, numericality: true
+  validates :user_id, presence: true, numericality: true
 end
