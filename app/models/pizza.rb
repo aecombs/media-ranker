2b6c3e3148a3dee71500728475fd02ebb37294ad
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-25 20:17:45 -0700

    Added correct logic to get_top_ten and get_spotlight

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -4,19 +4,18 @@ class Pizza < ApplicationRecord
   validates :crust, presence: true
   validates :name, presence: true, uniqueness: true
 
-  def self.get_top_ten(crust)
-    pizzas_of_crust_type = Pizza.where(crust: crust)
-    # if pizzas_of_crust_type.first.....has votes
-    #   return pizzas_of_crust_type.order("votes DESC").limit(10)
-    # else
-      return pizzas_of_crust_type.order("id DESC").limit(10)
-    # end
+  def self.get_top_ten(crust = nil)
+    if crust
+      pizzas = Pizza.where(crust: crust)
+    else
+      pizzas = Pizza.all
+    end
+    
+    top_pizzas = pizzas.sort_by { |p| p.votes.length }
+    return top_pizzas.reverse[0..9]
   end
 
   def self.get_spotlight
-    #TODO: change id DESC to votes DESC
-    top_ten_pizzas = Pizza.all.order("id DESC").limit(10)
-    #TODO: randomly select of the top ten votes?
-    return top_ten_pizzas.first
+    return self.get_top_ten.sample
   end
 end
